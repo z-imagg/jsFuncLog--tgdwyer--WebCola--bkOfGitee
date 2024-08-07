8b5ece3d7494f6b7f39b252ce7bdf0101c40a3da
prgrmz07 <prgrmz07@163.com>
2024-08-07 09:03:46 +0800

    [整理] [函数进入、返回 配置]  忽略方法名列表,  函数返回时是否打印参数们(默认是)

--- WebCola/src/_func_log_cfg.ts
+++ WebCola/src/_func_log_cfg.ts
@@ -1,20 +1,23 @@
 //函数进入、返回 配置
 
 //配置:忽略方法名列表
-const _cfgName_methodIgnore_localStorage:string="srcFilePath_classMethodName__list@WebCola@_func_log@localStorage";//string[]
+const _cfg_IgnoreMethodLs:string="_cfg_IgnoreMethodLs@_func_log@localStorage";//string[]
 export function isIgnore_methodFullName(srcFilePath:string,classMethodName:string):boolean{
-  const srcFilePath_classMethodName__list__jsonText:string=localStorage.getItem(_cfgName_methodIgnore_localStorage);
-  const srcFilePath_classMethodName__list:string[]=JSON.parse(srcFilePath_classMethodName__list__jsonText)
-  const item:string=`${srcFilePath}#${classMethodName}`
-  const has:boolean = item in srcFilePath_classMethodName__list; 
+  const ignoreMethodLs_jsonText:string|null=localStorage.getItem(_cfg_IgnoreMethodLs);
+  if(!ignoreMethodLs_jsonText){
+    return false;
+  }
+  const ignoreMethodLs:string[]=JSON.parse(ignoreMethodLs_jsonText)
+  const thisMethodFullName:string=`${srcFilePath}#${classMethodName}`
+  const has:boolean = thisMethodFullName in ignoreMethodLs; 
   const ignore:boolean = ! has;
   return ignore;
 }
 
 //配置:函数返回时是否打印参数们(默认是)
-const _cfgName_printLog_argsWhenFuncRet:string="printLog_argsWhenFuncRet@WebCola@_func_log@localStorage"; // 'true' or 'false'
+const _cfg_printArgsWhenFuncRet:string="printArgsWhenFuncRet@WebCola@_func_log@localStorage"; // 'true' or 'false'
 export function printLog_argsWhenFuncRet( ):boolean{
-  const printLog_argsWhenFuncRet:string=localStorage.getItem(_cfgName_printLog_argsWhenFuncRet);
+  const printLog_argsWhenFuncRet:string|null=localStorage.getItem(_cfg_printArgsWhenFuncRet);
   const do_:boolean = printLog_argsWhenFuncRet=='true'
   return do_;
 }
