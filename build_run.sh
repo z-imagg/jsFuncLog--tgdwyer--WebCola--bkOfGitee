e4063f78a2b9ec8ce7c49a4452c99a00fcf2540c
prgrmz07 <prgrmz07@163.com>
2024-08-08 20:03:24 +0800

    依赖安装

--- build_run.sh
+++ build_run.sh
@@ -2,13 +2,15 @@
 
 #[描述] 编译、运行 脚本
 
+set -e -u
+
 PrjDir=/app2/WebCola
 
 [[ ! -d $PrjDir/.node_env_v20.15.1/ ]] && bash /app/bash-simplify/nodejs_script/new_PrjNodejsEnv_by_nodeenv.sh $PrjDir  20.15.1
 
 source PrjNodeJsEnvActivate.sh
-pnpm install
-pnpm install /app2/js_func_log/
+npm install
+npm install /app2/js_func_log/
 
-pnpm run test  
+npm run test  
 
