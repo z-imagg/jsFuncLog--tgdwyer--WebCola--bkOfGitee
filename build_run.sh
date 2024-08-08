78868438190ba7f14659a6714bb8b65098db8499
prgrmz07 <prgrmz07@163.com>
2024-08-08 19:52:09 +0800

    xxx

---
+++ build_run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#[描述] 编译、运行 脚本
+
+PrjDir=/app2/WebCola
+
+[[ ! -d $PrjDir/.node_env_v20.15.1/ ]] && bash /app/bash-simplify/nodejs_script/new_PrjNodejsEnv_by_nodeenv.sh $PrjDir  20.15.1
+
+source PrjNodeJsEnvActivate.sh
+pnpm install
+pnpm install /app2/js_func_log/
+
+pnpm run test  
+
